9cbf4dcf3d5105f8b0de1a4453f66bd6250e7060
NjirainiG <167361730+NjirainiG@users.noreply.github.com>
2025-05-09 16:59:42 +0300

    Delete error_handler.py

--- error_handler.py
+++
@@ -1,7 +0,0 @@
-class GeoAIError(Exception):
-    """Base exception class for GeoAI application errors"""
-    def __init__(self, message: str, status_code: int = 400):
-        self.message = message
-        self.status_code = status_code
-        super().__init__(message)
-
